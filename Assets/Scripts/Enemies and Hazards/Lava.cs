11302275faf96db5602bcf781c82f995df035e14
Elijah L <Elijah2.Love@live.uwe.ac.uk>
2024-10-30 12:13:43 +0000

    work on underworld, added lava

---
+++ Assets/Scripts/Enemies and Hazards/Lava.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lava : MonoBehaviour
+{
+    float damage_timer;
+    [SerializeField] float time_to_damage;
+
+    [SerializeField] int damage;
+
+    void Start()
+    {
+        damage_timer = time_to_damage;
+    }
+
+    void Update()
+    {
+        if (damage_timer > 0.0f)
+        {
+            damage_timer -= Time.deltaTime;
+        }
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            if (damage_timer <= 0)
+            {
+                other.GetComponent<PlayerHealth>().takeDamage(damage);
+
+                damage_timer = time_to_damage;
+            }
+        }
+    }
+}
