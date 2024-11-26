558dc24927c7b8e280161cd5ace15cd2e724c491
Elijah L <Elijah2.Love@live.uwe.ac.uk>
2024-11-26 11:11:27 +0000

    work on twitch integration

--- Assets/Scripts/Misc/TwitchConnect.cs
+++ Assets/Scripts/Misc/TwitchConnect.cs
@@ -1,18 +1,62 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Net.Sockets;
+using System.IO;
 
 public class TwitchConnect : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    TcpClient twitch;
+    StreamReader reader;
+    StreamWriter writer;
+
+    const string URL = "irc.chat.twitch.tv";
+    const int PORT = 6667;
+
+    string user = "elijah12853";
+    string OAuth = "oauth:5i5vgk3dwkb2sbq1coew87jnp5j3sv";
+    string channel = "elijah12853";
+
+    float ping_counter = 0.0f;
+
+    void connectToTwitch()
     {
-        
+        twitch = new TcpClient(URL, PORT);
+        reader = new StreamReader(twitch.GetStream());
+        writer = new StreamWriter(twitch.GetStream());
+
+        writer.WriteLine("PASS " + OAuth);
+        writer.WriteLine("NICK " + user.ToLower());
+        writer.WriteLine("JOIN #" + channel.ToLower());
+        writer.Flush();
     }
 
-    // Update is called once per frame
+    void Awake()
+    {
+        connectToTwitch();
+    }
+
     void Update()
     {
-        
+        ping_counter += Time.deltaTime;
+
+        if (ping_counter > 60.0f)
+        {
+            writer.WriteLine("PING " + URL);
+            writer.Flush();
+            ping_counter = 0.0f;
+        }
+
+        if (!twitch.Connected)
+        {
+            connectToTwitch();
+        }
+
+        if (twitch.Available > 0)
+        {
+            string message = reader.ReadLine();
+
+            print(message);
+        }
     }
 }
